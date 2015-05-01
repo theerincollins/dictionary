585bdd0468d9430fa704836db3b77ce83eb15887
therincollins <erincollinsthethird@gmail.com>
2015-05-01 10:52:53 -0700

    Add word views page, and new word form for adding a new word to the dictionary. Add post method for the new word.

--- app.rb
+++ app.rb
@@ -5,10 +5,22 @@ also_reload('lib/**/*.rb')
 require('pry')
 
 get('/') do
-  test_word1 = Word.new("computer")
-  test_word1.save()
-  test_word2 = Word.new('mouse')
-  test_word2.save()
   @words = Word.all
   erb(:index)
+end
+
+get('/word/new') do
+  erb(:new_word)
+end
+
+post('/save_word') do
+  new_word = Word.new(params.fetch("word_name"))
+  new_word.save()
+  @words = Word.all
+  erb(:index)
+end
+
+get('/word/:id') do
+  @word = Word.find(params.fetch('id').to_i())
+  erb(:word)
 end
